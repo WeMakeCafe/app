f4dc2344e78d82e5b1fd2318036ad894ac71de83
hiroze90 <hiroze90@gmail.com>
2022-04-04 22:21:50 +0900

    마이페이지 작성 중

--- app/src/main/java/com/example/wmc/MainActivity.java
+++ app/src/main/java/com/example/wmc/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends FragmentActivity { //상속 fragment로 변경
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_list);  //페이지 확인하기
+        setContentView(R.layout.fragment_mypage);  //페이지 확인하기
 /*        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
