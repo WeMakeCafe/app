8ab25b6a505f8d4b45d675cc1ab6e139d21c8317
Leejaeyoon <nhemch123@naver.com>
2022-04-12 00:16:12 +0900

    fragment_list_search페이지의 리싸이클러뷰 푸터 추가 및 recyclerview_footer페이지 생성

--- app/src/main/java/com/example/wmc/MainActivity.java
+++ app/src/main/java/com/example/wmc/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.wmc;
 import android.os.Bundle;
 import android.view.Window;
 
+import com.example.wmc.ui.Fragment.ListSearchAdapter;
+import com.example.wmc.ui.Fragment.ListSearchItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,19 +13,24 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wmc.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-//
+
 //    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.item_more_review);  //페이지 확인하기
+        setContentView(R.layout.fragment_list_search);  //페이지 확인하기
 
+        bindList(); // fragment_list_search 페이지 리사이클러뷰 푸터 실험용 코드
 //        binding = ActivityMainBinding.inflate(getLayoutInflater());
 //        setContentView(binding.getRoot());
 //
@@ -34,5 +41,30 @@ public class MainActivity extends AppCompatActivity {
 //
 //        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //        NavigationUI.setupWithNavController(binding.navViewMain, navController);
+    }
+
+    private void bindList() {         // fragment_list_search 페이지 리사이클러뷰 푸터 실험용 코드
+        // List item 생성
+        ArrayList<ListSearchItem> person = new ArrayList<>();
+
+        person.add(new ListSearchItem("이디야커피 수원대점", "경기 화성시 봉담읍 와우안길 16"));
+        person.add(new ListSearchItem("와우당", "경기 화성시 봉담읍 와우안길 10"));
+        person.add(new ListSearchItem("-", "서울특별시 강남구 역삼동 논현로 539"));
+        person.add(new ListSearchItem("옹느세자메", "서울특별시 용산구 이태원로54길 51"));
+        person.add(new ListSearchItem("이디야커피 수원대점", "경기 화성시 봉담읍 와우안길 16"));
+        person.add(new ListSearchItem("와우당", "경기 화성시 봉담읍 와우안길 10"));
+        person.add(new ListSearchItem("-", "서울특별시 강남구 역삼동 논현로 539"));
+        person.add(new ListSearchItem("옹느세자메", "서울특별시 용산구 이태원로54길 51"));
+
+        // Recycler view
+        RecyclerView recyclerView = findViewById(R.id.recent_searches_recyclerView);
+
+        // Adapter 추가
+        ListSearchAdapter adapter = new ListSearchAdapter(person);
+        recyclerView.setAdapter(adapter);
+
+        // Layout manager 추가
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
