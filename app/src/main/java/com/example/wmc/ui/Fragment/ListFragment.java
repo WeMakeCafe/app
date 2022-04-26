aa4f78fdf40af60fedc3bce20999290fcb0d6e90
Leejaeyoon <nhemch123@naver.com>
2022-04-27 01:44:36 +0900

    ListFragment버튼 리스너 추가
    //검색창, 돋보기 클릭 시, 리스너 추가

--- app/src/main/java/com/example/wmc/ui/Fragment/ListFragment.java
+++ app/src/main/java/com/example/wmc/ui/Fragment/ListFragment.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +28,8 @@ public class ListFragment extends Fragment {
     private FragmentListBinding binding;    // FragmentListBinding 변수이름 Ctrl + 좌클릭하면 연결된 .xml로 이동함
     private static NavController navController;
     Button add;
+    Button searchText;
+    Button searchButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,12 +37,28 @@ public class ListFragment extends Fragment {
         binding = FragmentListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         add = root.findViewById(R.id.add_cafe_button);
+        searchText = root.findViewById(R.id.cafe_search_input);
+        searchButton = root.findViewById(R.id.search_button);
 
-        add.setOnClickListener(new View.OnClickListener() {
+        add.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext().getApplicationContext(), "추가버튼 클릭", Toast.LENGTH_SHORT).show();
                 navController.navigate(R.id.list_to_cafe_registration);
+            }
+        });
+
+        searchText.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.list_to_list_search);
+            }
+        });
+
+        searchButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.list_to_list_search);
             }
         });
 
