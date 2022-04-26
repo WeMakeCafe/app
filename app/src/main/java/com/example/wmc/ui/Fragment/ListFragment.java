cbf457c82db25031d2392a9d06f05f6f4f494ba3
Leejaeyoon <nhemch123@naver.com>
2022-04-26 18:04:10 +0900

    ListFragment 네비게이션 연결

--- app/src/main/java/com/example/wmc/ui/Fragment/ListFragment.java
+++ app/src/main/java/com/example/wmc/ui/Fragment/ListFragment.java
@@ -1,28 +1,55 @@
 package com.example.wmc.ui.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.wmc.R;
 import com.example.wmc.databinding.FragmentListBinding;
 
 public class ListFragment extends Fragment {
 
     private FragmentListBinding binding;    // FragmentListBinding 변수이름 Ctrl + 좌클릭하면 연결된 .xml로 이동함
+    private static NavController navController;
+    Button add;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        add = root.findViewById(R.id.add_cafe_button);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext().getApplicationContext(), "추가버튼 클릭", Toast.LENGTH_SHORT).show();
+
+                navController.navigate(R.id.list_to_cafe_registration);
+            }
+        });
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
     }
 
     @Override
