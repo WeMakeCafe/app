b53a49ab9f6cdea40a6ecef4f546cc12ebf2e868
hiroze90 <hiroze90@gmail.com>
2022-04-27 01:13:25 +0900

    카페추가페이지 -> 태그추가페이지 버튼 연결
    //키보드 올라올때 하단바 함께 올라오는것 수정.
    //AndroidManifest.xml에 해당 내용 있음

--- app/src/main/java/com/example/wmc/ui/Fragment/CafeRegistrationFragment.java
+++ app/src/main/java/com/example/wmc/ui/Fragment/CafeRegistrationFragment.java
@@ -4,24 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.wmc.R;
 import com.example.wmc.databinding.FragmentCafeRegistrationBinding;
 
 public class CafeRegistrationFragment extends Fragment {
+
     private FragmentCafeRegistrationBinding binding;
+    private static NavController navController;
+    Button tag;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentCafeRegistrationBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        tag = root.findViewById(R.id.add_tag_button);
 
-
+        tag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.navigation_review_tag);
+            }
+        });
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
     }
 
     @Override
