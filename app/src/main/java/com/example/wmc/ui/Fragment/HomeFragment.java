38a5e6061941e89143fe9ba70ef2e48934169a0e
Leejaeyoon <nhemch123@naver.com>
2022-04-28 19:27:52 +0900

    HomeFragment에서 2순위 해시태그의 카페목록 리싸이클러뷰 자바코드 생성

--- app/src/main/java/com/example/wmc/ui/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/wmc/ui/Fragment/HomeFragment.java
@@ -19,6 +19,8 @@ import com.example.wmc.HomeFavorite.HomeFavoriteAdapter;
 import com.example.wmc.HomeFavorite.HomeFavoriteItem;
 import com.example.wmc.HomeTag1.HomeTag1Adapter;
 import com.example.wmc.HomeTag1.HomeTag1Item;
+import com.example.wmc.HomeTag2.HomeTag2Adapter;
+import com.example.wmc.HomeTag2.HomeTag2Item;
 import com.example.wmc.R;
 import com.example.wmc.databinding.FragmentHomeBinding;
 
@@ -63,6 +65,7 @@ public class HomeFragment extends Fragment {
         });
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Home에서 1순위 해시태그에 대한 리싸이클러뷰 작성
 
         ArrayList<HomeTag1Item> homeTag1Items = new ArrayList<>();
 
@@ -93,6 +96,38 @@ public class HomeFragment extends Fragment {
             }
         });
 
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Home에서 2순위 해시태그에 대한 리싸이클러뷰 작성
+
+        ArrayList<HomeTag2Item> homeTag2Items = new ArrayList<>();
+
+        homeTag2Items.add(new HomeTag2Item("뺵다방 홍대점", "서울시 ~~~~ 167", "#가성비", "#신나는", "#디저트",
+                "테이블이 매우 협소합니다. \n" + "하지만, 가격이 매우 저렴하고 맛있습니다!\n" + "마카롱이 진짜 최고에요ㅠ", R.drawable.logo, 1));
+        homeTag2Items.add(new HomeTag2Item("잇츠커피 수원대점", "경기도 화성시 와우리 42-11", "#화장실", "#애견", "#레트로",
+                "테이블이 협소해서 공부하기는 어렵지만\n" + "노래도 나오고 친구들이랑 같이 이야기하기에는좋아요.", R.drawable.logo, 3));
+        homeTag2Items.add(new HomeTag2Item("스타벅스 수원역점", "경기도 수원시 수원역 ~~~ 84-4", "#분위기", "#스터디", "#감성",
+                "징짜 맛있음\n징짜 맛있음\n징짜 맛있음", R.drawable.logo_v2, 4));
+
+
+        // Recycler view
+        RecyclerView tag2RecyclerView = root.findViewById(R.id.second_recyclerView);
+
+        // Adapter 추가
+        HomeTag2Adapter tag2Adapter = new HomeTag2Adapter(homeTag2Items);
+        tag2RecyclerView.setAdapter(tag2Adapter);
+
+        // Layout manager 추가
+        LinearLayoutManager tag2LayoutManager = new LinearLayoutManager(getContext().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        tag2RecyclerView.setLayoutManager(tag2LayoutManager);
+
+        tag2Adapter.setOnItemClickListener_HomeTag2(new HomeTag2Adapter.OnItemClickEventListener_HomeTag2() {
+            @Override
+            public void onItemClick(View a_view, int a_position) {
+                final HomeTag2Item item = homeTag2Items.get(a_position);
+                Toast.makeText(getContext().getApplicationContext(), item.getCafeName() + " 클릭됨.", Toast.LENGTH_SHORT).show();
+            }
+        });
         return root;
     }
 
