736d3b7a09853fa87044fdf659a63222e4bba746
Leejaeyoon <nhemch123@naver.com>
2022-05-01 16:07:17 +0900

    listSearch에서 최근검색 리싸이클러뷰 자바 코드 생성

--- app/src/main/java/com/example/wmc/ui/Fragment/ListSearchFragment.java
+++ app/src/main/java/com/example/wmc/ui/Fragment/ListSearchFragment.java
@@ -8,15 +8,25 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wmc.CafeRegistration.CafeRegistrationAdapter;
+import com.example.wmc.CafeRegistration.CafeRegistrationItem;
+import com.example.wmc.HomeFavorite.HomeFavoriteItem;
+import com.example.wmc.ListSearch.ListSearchAdapter;
+import com.example.wmc.ListSearch.ListSearchItem;
 import com.example.wmc.R;
 import com.example.wmc.databinding.FragmentListSearchBinding;
+
+import java.util.ArrayList;
 
 public class ListSearchFragment extends Fragment {
 
@@ -44,6 +54,35 @@ public class ListSearchFragment extends Fragment {
             public void onClick(View v) {
 //                navController.navigate(R.id.);    // 돋보기 버튼 클릭 시, ListCafelistFragment로 이동
                                                     // 아직 Navigation이 없음
+            }
+        });
+
+        // 최근 검색 리싸이클러뷰
+        ArrayList<ListSearchItem> listSearchItems = new ArrayList<>();
+
+        listSearchItems.add(new ListSearchItem("이디야커피 수원대점", "수원대학교 1203~~~ 와우리 42-15"));
+        listSearchItems.add(new ListSearchItem("할리스커피 수원대점", "수원대학교 1203~~~ 와우리 11-13"));
+        listSearchItems.add(new ListSearchItem("와우당 수원대점", "경기도 화성시 1203~~~ 와우리 331-53"));
+        listSearchItems.add(new ListSearchItem("메가커피 홍대점", "서울시 ㅇ~~ 홍대~~ 11-17"));
+        listSearchItems.add(new ListSearchItem("백다방 탑동우방점", "경기도 수원시 권선구 탑동 ~~ 12-37"));
+        listSearchItems.add(new ListSearchItem("스타벅스 수원영통점", "경기도 수원시 영통구 ~~ 판타지움 11-1"));
+
+        // Adapter 추가
+        RecyclerView listSearchRecyclerView = root.findViewById(R.id.recent_searches_recyclerView);
+
+        ListSearchAdapter listSearchAdapter = new ListSearchAdapter(listSearchItems);
+        listSearchRecyclerView.setAdapter(listSearchAdapter);
+
+        // Layout manager 추가
+        LinearLayoutManager listSearchLayoutManager = new LinearLayoutManager(getContext().getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        listSearchRecyclerView.setLayoutManager(listSearchLayoutManager);
+
+        listSearchAdapter.setOnItemClickListener_listSearch(new ListSearchAdapter.OnItemClickEventListener_listSearch() {
+            @Override
+            public void onItemClick(View a_view, int a_position) {
+                final ListSearchItem item = listSearchItems.get(a_position);
+                Toast.makeText(getContext().getApplicationContext(), item.getSearch_cafeName() + " 클릭됨.", Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.list_search_to_cafe_detail);
             }
         });
 
