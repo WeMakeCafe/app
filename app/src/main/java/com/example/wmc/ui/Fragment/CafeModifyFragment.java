c0321dd4f23499d3405872098890478eb8360cb2
Leejaeyoon <nhemch123@naver.com>
2022-05-03 19:01:58 +0900

    CafeModify페이지 네비게이션 추가

--- app/src/main/java/com/example/wmc/ui/Fragment/CafeModifyFragment.java
+++ app/src/main/java/com/example/wmc/ui/Fragment/CafeModifyFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,14 +26,47 @@ import java.util.ArrayList;
 public class CafeModifyFragment extends Fragment {
     private FragmentCafeModifyBinding binding;
     private static NavController navController;
+    Button add_image_button;
+    Button modify_button;
+    TextView request_deletion_textView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentCafeModifyBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        add_image_button = root.findViewById(R.id.add_image_button);
+        modify_button = root.findViewById(R.id.modify_button);
+        request_deletion_textView = root.findViewById(R.id.request_deletion_textView);
+
+        // 이미지 수정 + 버튼 클릭 시,
+        add_image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 갤러리로 이동
+            }
+        });
 
 
+        // 수정하기 버튼 클릭 시,
+        modify_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.cafe_modify_to_cafe_detail);    // 내가 수정한 카페디테일로 이동
+            }
+        });
+
+
+        // 삭제요청 버튼 클릭 시,
+        request_deletion_textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.cafe_modify_to_cafe_delete);    // 삭제 요청 페이지로 이동
+            }
+        });
+
+
+        // 이미지 수정 리싸이클러뷰
         ArrayList<CafeModifyItem> modifyImageItems = new ArrayList<>();
 
         modifyImageItems.add(new CafeModifyItem(R.drawable.logo));
