c2acc8b6e6952b5149d576467e79b1756e62f2ed
hiroze90 <hiroze90@gmail.com>
2022-05-07 06:18:43 +0900

    ReviewTagFragment.java 누더기 코드 약식 정리 버전

--- app/src/main/java/com/example/wmc/ui/Fragment/ReviewTagFragment.java
+++ app/src/main/java/com/example/wmc/ui/Fragment/ReviewTagFragment.java
@@ -39,28 +39,13 @@ public class ReviewTagFragment extends Fragment {
 //        ConstraintLayout cons1 = root.findViewById(R.id.constrant_taste);
 //        ConstraintLayout cons2 = root.findViewById(R.id.constrant_feel);
 //        ConstraintLayout cons3 = root.findViewById(R.id.constrant_service);
-//
-//        ImageView view1 = root.findViewById(R.id.tasteimage8);
-//        ImageView view2 = root.findViewById(R.id.feelingImage8);
-//        ImageView view3 = root.findViewById(R.id.serviceimage8);
-//
-//        ImageView view4 = root.findViewById(R.id.tasteimage7);
-//        ImageView view5 = root.findViewById(R.id.feelingImage7);
-//        ImageView view6 = root.findViewById(R.id.serviceimage7);
-//
-//        ImageView view7 = root.findViewById(R.id.tasteimage6);
-//        ImageView view8 = root.findViewById(R.id.feelingimage6);
-//        ImageView view9 = root.findViewById(R.id.serviceImage6);
-//
-        TextView addTag1 = root.findViewById(R.id.addTag1); // 선택한 태그1
-        TextView addTag2 = root.findViewById(R.id.addTag2); // 선택한 태그2
-        TextView addTag3 = root.findViewById(R.id.addTag3); // 선택한 태그3
-        Button tag1_delete_button = root.findViewById(R.id.tag1_delete_button); // 선택한 태그1의 X버튼
-        Button tag2_delete_button = root.findViewById(R.id.tag2_delete_button); // 선택한 태그2의 X버튼
-        Button tag3_delete_button = root.findViewById(R.id.tag3_delete_button); // 선택한 태그3의 X버튼
-//        Button tag1 = root.findViewById(R.id.tag1);
-//        Button tag2 = root.findViewById(R.id.tag2);
-//        Button tag3 = root.findViewById(R.id.tag3);
+
+        TextView addTag1 = root.findViewById(R.id.addTag1); // 아래 태그 버튼 3개 중 1
+        TextView addTag2 = root.findViewById(R.id.addTag2); // 아래 태그 버튼 3개 중 2
+        TextView addTag3 = root.findViewById(R.id.addTag3); // 아래 태그 버튼 3개 중 3
+        Button tag1_delete_button = root.findViewById(R.id.tag1_delete_button); // 아래 태그 버튼 3개의 X버튼
+        Button tag2_delete_button = root.findViewById(R.id.tag2_delete_button); // 아래 태그 버튼 3개의 X버튼
+        Button tag3_delete_button = root.findViewById(R.id.tag3_delete_button); // 아래 태그 버튼 3개의 X버튼
 //
 //        view1.setOnClickListener(new View.OnClickListener() {
 //            @Override
