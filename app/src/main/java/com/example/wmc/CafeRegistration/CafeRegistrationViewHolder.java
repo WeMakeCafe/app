aa0fd838f7f9ed12701d84369506a6805f7bab13
Leejaeyoon <nhemch123@naver.com>
2022-05-08 19:47:38 +0900

    CafeRegistration에서 갤러리에서 이미지 가져오기

--- app/src/main/java/com/example/wmc/CafeRegistration/CafeRegistrationViewHolder.java
+++ app/src/main/java/com/example/wmc/CafeRegistration/CafeRegistrationViewHolder.java
@@ -12,25 +12,36 @@ import com.example.wmc.R;
 
 public class CafeRegistrationViewHolder extends RecyclerView.ViewHolder{
 
-    ImageView add_modify_imageView;
+    ImageView image;
     Button imageDeleteButton;
 
-    public static int CAFEREGISTRATION_VIEW_TYPE = R.layout.item_add_image;
+    CafeRegistrationViewHolder(View itemView) {
+        super(itemView) ;
 
-    public CafeRegistrationViewHolder(@NonNull View itemView, final CafeRegistrationAdapter.OnItemClickEventListener_CafeRegistration a_itemClickListener) {
-        super(itemView);
-
-        add_modify_imageView = itemView.findViewById(R.id.add_modify_imageView);
+        // 뷰 객체에 대한 참조.
+        image = itemView.findViewById(R.id.add_modify_imageView);
         imageDeleteButton = itemView.findViewById(R.id.imageDeleteButton);
-
-        itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View a_view) {
-                final int position = getAdapterPosition();
-                if (position != RecyclerView.NO_POSITION) {
-                    a_itemClickListener.onItemClick(a_view, position);
-                }
-            }
-        });
     }
+
+//    ImageView add_modify_imageView;
+//    Button imageDeleteButton;
+//
+//    public static int CAFEREGISTRATION_VIEW_TYPE = R.layout.item_add_image;
+//
+//    public CafeRegistrationViewHolder(@NonNull View itemView, final CafeRegistrationAdapter.OnItemClickEventListener_CafeRegistration a_itemClickListener) {
+//        super(itemView);
+//
+//        add_modify_imageView = itemView.findViewById(R.id.add_modify_imageView);
+//        imageDeleteButton = itemView.findViewById(R.id.imageDeleteButton);
+//
+//        itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View a_view) {
+//                final int position = getAdapterPosition();
+//                if (position != RecyclerView.NO_POSITION) {
+//                    a_itemClickListener.onItemClick(a_view, position);
+//                }
+//            }
+//        });
+//    }
 }
